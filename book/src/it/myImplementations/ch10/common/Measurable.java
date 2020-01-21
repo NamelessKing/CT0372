5d629088fd210d49383a65db7765d7f4f34aa67f
Tanjin Habibur <tanjin.habibur@dbalab.it>
2020-01-21 14:07:13 +0100

    completed ex10.1 ->ch10

---
+++ book/src/it/myImplementations/ch10/common/Measurable.java
@@ -0,0 +1,5 @@
+package it.myImplementations.ch10.common;
+
+public interface Measurable {
+    double getMeasure();
+}
---
+++ book/src/it/myImplementations/ch10/e_10_1/Main_e_10_1.java
@@ -0,0 +1,24 @@
+package it.myImplementations.ch10.e_10_1;
+
+import it.myImplementations.ch10.common.Data;
+import it.myImplementations.ch10.common.Measurable;
+
+public class Main_e_10_1 {
+
+    public static void main(String[] args) {
+
+        Measurable[] accounts = new Measurable[3];
+        accounts[0] = new BankAccount(4000);
+        accounts[1] = new BankAccount(17525);
+        accounts[2] = new BankAccount(19200);
+
+        double averageBalance = Data.average(accounts);
+        System.out.println("Average balance: " + averageBalance);
+        System.out.println("Expected: 13575");
+
+        double largestBalance = Data.max(accounts).getMeasure();
+        System.out.println("Largest Balance: " + largestBalance);
+        System.out.println("Expected: 19200");
+
+    }
+}
